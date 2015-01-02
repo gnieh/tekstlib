b2e0ab50142727d63eaa973bb14b8f6fabc1ba9f
Lucas Satabin <lucas.satabin@gnieh.org>
2015-01-02 18:54:49 +0100

    Add GapBuffer implementation

---
+++ src/main/scala/gnieh/string/GapBuffer.scala
@@ -0,0 +1,137 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package gnieh
+package string
+
+import scala.annotation.tailrec
+
+/** An implementation of a gap buffer.
+ *
+ *  @author Lucas Satabin
+ */
+class GapBuffer(initialSize: Int) {
+
+  // the buffer itself
+  private var _buffer = Array.ofDim[Char](initialSize)
+
+  // the current gap start index (included)
+  private var _gapStart = 0
+
+  // the current buffer size
+  private def _size = _buffer.size
+
+  // the current gap end index (excluded)
+  private var _gapEnd = _size
+
+  // the size of the current gap
+  private def gapSize =
+    _gapEnd - _gapStart
+
+  /** Returns the index of the current cursor position. */
+  def cursor: Int =
+    _gapStart
+
+  /** Sets the cursor at the given position. */
+  def cursor_=(pos: Int): Unit =
+    moveBy(pos - cursor)
+
+  /** Returns the size of the buffer. */
+  def bufferSize: Int =
+    _size
+
+  /** Returns the size of the buffer content. */
+  def contentSize: Int =
+    _size - (_gapEnd - _gapStart)
+
+  /** Moves the cursor by the given amount of characters.
+   *  A positive `delta` moves the cursor to the right, a negative one moves
+   *  it to the left. */
+  def moveBy(delta: Int): Unit =
+    if(_gapStart + delta > contentSize || _gapStart + delta < 0) {
+      // check delta bounds
+      throw new Exception(f"Cursor index out of buffer: ${cursor + delta}")
+    } else if(delta != 0) {
+      val oldStart = _gapStart
+      val oldEnd = _gapEnd
+      // compute the new gap position and size
+      _gapStart +=  delta
+      _gapEnd = math.min(_gapEnd + delta, _size)
+      if(delta > 0) {
+        // moved to the right
+        System.arraycopy(_buffer, oldEnd, _buffer, oldStart, delta)
+      } else {
+        // moved to the left
+        System.arraycopy(_buffer, oldStart + delta, _buffer, oldEnd + delta, -delta)
+      }
+      regap()
+    }
+
+    /** Insert the given character at the current cursor position */
+    def insert(c: Char): Unit = {
+      _buffer(_gapStart) = c
+      _gapStart += 1
+      regap()
+    }
+
+    /** Inserts the given string at the current cursor position */
+    def insert(s: String): Unit = {
+      @tailrec
+      def loop(s: String): Unit =
+        if(s.size <= gapSize) {
+          s.getChars(0, s.size, _buffer, _gapStart)
+          _gapStart += s.size
+          regap()
+        } else {
+          val size = gapSize
+          s.getChars(0, size, _buffer, _gapStart)
+          _gapStart += size
+          regap()
+          loop(s.substring(size))
+        }
+      loop(s)
+    }
+
+    /** Returns the content of this buffer as a string. */
+    override def toString: String =
+      if(gapSize == 0) {
+        new String(_buffer)
+      } else {
+        val start = new String(_buffer, 0, _gapStart)
+        val end = new String(_buffer, _gapEnd, _size - _gapEnd)
+        start + end
+      }
+
+    // recreate a gap, expanding the buffer as needed
+    private def regap(): Unit = if(gapSize == 0) {
+      // only regap if gap is empty
+
+      val newSize = math.min(_size.toLong * 3 / 2, Int.MaxValue).toInt
+
+      if(newSize == _size)
+        throw new Exception("Buffer is full")
+
+      val newBuffer = Array.ofDim[Char](newSize)
+
+      val endLength = _size - _gapEnd
+
+      System.arraycopy(_buffer, 0, newBuffer, 0, _gapStart)
+      System.arraycopy(_buffer, _gapEnd, newBuffer, newSize - endLength, endLength)
+
+      _gapEnd = newSize - endLength
+
+      _buffer = newBuffer
+
+    }
+
+}
---
+++ src/test/scala/gnieh/string/GapBufferTest.scala
@@ -0,0 +1,123 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package gnieh.string
+
+import org.scalatest._
+
+class GapBufferTest extends FlatSpec with ShouldMatchers {
+
+  "The initial content size" should "be empty" in {
+    val gb = new GapBuffer(1024)
+
+    gb.bufferSize should be(1024)
+    gb.contentSize should be(0)
+    gb.toString should be("")
+
+    gb.cursor should be(0)
+
+  }
+
+  "Inserting text that fits into the gap" should "not modify the buffer size" in {
+
+    val gb = new GapBuffer(10)
+
+    gb.insert('1')
+    gb.bufferSize should be(10)
+    gb.contentSize should be(1)
+
+    gb.insert("23456789")
+    gb.bufferSize should be(10)
+    gb.contentSize should be(9)
+    gb.toString should be("123456789")
+
+  }
+
+  it should "modify the buffer size if the gap is empty after insertion" in {
+
+    val gb = new GapBuffer(10)
+
+    gb.insert('1')
+    gb.bufferSize should be(10)
+    gb.contentSize should be(1)
+
+    gb.insert("234567890")
+    gb.bufferSize should be(15)
+    gb.contentSize should be(10)
+    gb.toString should be("1234567890")
+
+  }
+
+  "Inserting a text that does not fit into the gap" should "result in buffer expansion" in {
+
+    val gb = new GapBuffer(10)
+
+    gb.insert("123456789012345678901234567890")
+
+    gb.bufferSize should be(33)
+    gb.contentSize should be(30)
+    gb.toString should be("123456789012345678901234567890")
+
+  }
+
+  "Cursor" should "initially be at position 0" in {
+
+    val gb = new GapBuffer(10)
+
+    gb.cursor should be(0)
+
+  }
+
+  it should "be moved after the insert text" in {
+
+    val gb = new GapBuffer(10)
+
+    gb.insert('1')
+    gb.cursor should be(1)
+
+    gb.insert("2345")
+    gb.cursor should be(5)
+
+    gb.insert("67890")
+    gb.cursor should be(10)
+
+  }
+
+  it should "be moved by the given delta" in {
+
+    val gb = new GapBuffer(10)
+    gb.insert("1234567890")
+
+    gb.moveBy(-5)
+    gb.cursor should be(5)
+
+    gb.moveBy(3)
+    gb.cursor should be(8)
+
+  }
+
+  it should "be the place where new content is added" in {
+
+    val gb = new GapBuffer(10)
+    gb.insert("123456789")
+
+    gb.moveBy(-4)
+
+    gb.insert("youpi")
+
+    gb.cursor should be(10)
+    gb.toString should be("12345youpi6789")
+
+  }
+
+}
